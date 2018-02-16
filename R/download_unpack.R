4a324b61c8290d8203c466fecf33a384186acc85
Jan Dietrich <dietrich@pik-potsdam.de>
2018-02-16 23:36:02 +0100

    download_unpack expects now options separate for each repository

--- R/download_unpack.R
+++ R/download_unpack.R
@@ -6,97 +6,77 @@
 #' cfg$input, cfg$repositories). Settings in the config list will be overwritten by other arguments of
 #' this function if they are not set to NULL
 #' @param targetdir directory the files should be downloaded and extracted to
-#' @param repositories a vector of repositories the files should be looked for. Files will be searched
-#' for in all repositories until they are found, always starting with the first repository in the 
-#' list.
-#' @param username username for accessing the repo (if required). If set to NULL will be tried to be set
-#' through getOption("username")
-#' @param ssh_private_keyfile path to private ssh key (if required). If set to NULL will be tried to be set
-#' through getOption("ssh_private_keyfile")
-#' @param ssh_public_keyfile path to public ssh key (if required). If set to NULL will be tried to be set
-#' through getOption("ssh_public_keyfile")
+#' @param repositories a list of repositories (please pay attention to the list format!) in which the files 
+#' should be searched for. Files will be searched in all repositories until found, always starting with the 
+#' first repository in the list. The argument must have the format of a named list with the url of the repository
+#' as name and a corresponding list of options such as username or password to access the repository as value. If
+#' no options are required the value has to be NULL. (e.g. 
+#' list("ftp://my_pw_protected_server.de/data"=list(user="me",password=12345), "http://free_server.de/dat"=NULL))
 #' @param debug switch for debug mode with additional diagnostic information
-#' @param ... further options provided to curl handle
 #' @author Jan Philipp Dietrich
 #' @importFrom utils untar
 #' @export
 
-download_unpack <- function(input, targetdir="input", repositories=NULL, username=NULL, ssh_private_keyfile=NULL, ssh_public_keyfile=NULL, debug=FALSE, ...) {
+download_unpack <- function(input, targetdir="input", repositories=NULL, debug=FALSE) {
   
   if(is.list(input)) {
     files <- input$input
     if(is.null(repositories))        repositories <- input$repositories
-    if(is.null(username))            username <- input$username
-    if(is.null(ssh_private_keyfile)) ssh_private_keyfile <- input$ssh_private_keyfile
-    if(is.null(ssh_public_keyfile))  ssh_public_keyfile <- input$ssh_public_keyfile
     if(is.null(debug))               debug <- input$debug
   } else {
     files <- input
   }
-  
-  if(is.null(username)) username <- getOption("username")
-  if(is.null(ssh_private_keyfile)) ssh_private_keyfile <- getOption("ssh_private_keyfile")
-  if(is.null(ssh_public_keyfile)) ssh_public_keyfile <- getOption("ssh_public_keyfile")
   
   if(!dir.exists(targetdir)) dir.create(targetdir)
   
   writeLines(files,paste0(targetdir,"/source_files.log"))
   
   # create curl handle
-  if(any(grepl("://",repositories))) {
+  if(any(grepl("://",names(repositories)))) {
     if(!requireNamespace("curl", quietly = TRUE)) stop("The package curl is required for downloading files!")
-    h <- curl::new_handle(verbose=debug)
-    if(!is.null(username)) try(curl::handle_setopt(h, username=username))
-    if(!is.null(ssh_private_keyfile)) try(curl::handle_setopt(h, ssh_private_keyfile=ssh_private_keyfile))
-    if(!is.null(ssh_public_keyfile)) try(curl::handle_setopt(h, ssh_public_keyfile=ssh_public_keyfile))
-    if(length(list(...))>0) try(curl::handle_setopt(h, ...))
   }
   
-  cat("Load data from repository\n")
-  anydatafound <- FALSE
+  .unpack <- function(file, filepath, repo, targetdir,found) {
+    message("    ",file)
+    untar(filepath,exdir=targetdir)
+    return(rbind(found,data.frame(row.names=file,repo=repo,md5sum=tools::md5sum(filepath))))
+  }
+  
+  message("Load and unpack data..")
   md5sum <- list()
-  for(i in 1:length(files)) {
-    file <- files[i]
-    cat(" Search for",file,"...\n")
-    filepath <- NULL
-    for(repo in repositories) {
-      cat("  Try",repo,"...")
+  found <- NULL
+  for(repo in names(repositories)) {
+    message("  try ",repo)
+    if(grepl("://",repo)) {
+      h <- try(curl::new_handle(verbose=debug, .list=repositories[[repo]]),silent = !debug)
+      tmp <- try(curl::curl_fetch_memory(repo, handle=h)$status_code, silent = !debug)
+      if(tmp != 200) {
+        message("    repository access failed .. skip repository!")
+        next
+      }
+    } else if(!file.exists(repo)) {
+      message("    repository access failed .. skip repository!")
+      next 
+    }
+    for(file in files) {
+      filepath <- NULL
       path <- paste0(sub("/$","",repo),"/",file)
-      if(grepl("://",path)) {
-        tmpdir <- tempdir()
-        if(debug) tmpdir <- targetdir
+      if(grepl("://",repo)) {
+        tmpdir <- ifelse(debug,targetdir,tempdir())
         tmp <- try(curl::curl_download(path,paste0(tmpdir,"/",file),handle=h),silent = !debug)
-        if("try-error" %in% class(tmp)) {
-          cat(" failed!\n")
-          if(debug) {
-            cat(tmp)
-          }
-        } else {
-          names(files)[i] <- repo
-          filepath <- paste0(tmpdir,"/",file)
-          cat(" success!\n")
-          break
+        if(!("try-error" %in% class(tmp))) {
+          files <- files[-match(file,files)]
+          found <- .unpack(file, paste0(tmpdir,"/",file), repo, targetdir, found)
         }
-      } else {
-        if(file.exists(path)) {
-          names(files)[i] <- repo
-          filepath <- path
-          cat(" success!\n")
-          break
-        } else {
-          cat(" failed!\n")
-        }
+      } else if(file.exists(path)) {
+        files <- files[-match(file,files)]
+        found <- .unpack(file, path, repo, targetdir,found)
       }
     }
-    if(is.null(filepath)) {
-      warning(file," could not be found in any of the specified repositories!")
-    } else {
-      anydatafound <- TRUE
-      md5sum[[files[i]]] <- tools::md5sum(filepath)
-      untar(filepath,exdir=targetdir)
-    }
+    if(length(files)==0) break
   }
-  if(!anydatafound) stop("None of the provided input data files could be found! In the case of remote access: Did you provide proper username and ssh_private_keyfile?")
-  attr(files,"md5") <- md5sum
-  return(files)
+  if(is.null(found)) stop("No file could be found!")
+  if(length(files)>0) warning("Some files not found:\n  ",paste(files, collapse = "\n  "))
+  message("..done")
+  return(found)
 }
