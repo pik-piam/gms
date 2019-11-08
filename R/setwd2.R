2d316d752d5d1bdb733dbb9e7a7e91fad94a35c5
Benjamin Leon Bodirsky <bodirsky@pik-potsdam.de>
2019-11-08 12:15:25 +0100

    bugfix. clipboard copy doesnt work for linux

--- R/setwd2.R
+++ R/setwd2.R
@@ -1,17 +1,17 @@
 #' setwd2
 #' 
-#' Mini function that copies in a path from the Clipboard and sets the directory to it.
+#' Mini function that allows you to set a directory based on a readline input.
 #' Very useful for Windows users, as it replaces backslashs by slashs.
 #' 
 #' @usage setwd2(return_only=FALSE)
 #' @param return_only if TRUE, the path is not changed, but the clipboard path is returned as string.
 #' @return if return_only=FALSE: Nothing, but the working directory is set to. Otherwhise: no working directory returned, but path transformed.
 #' @author Benjamin Leon Bodirsky
-#' @importFrom utils readClipboard
 #' @export
 
 setwd2<-function(return_only=FALSE){
-  x=readClipboard()
+  cat("Please enter URL here:")
+  x=readline()
   x = gsub("\\\\", "/", x)
   if (return_only==FALSE){
     setwd(x) 
