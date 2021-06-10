1602f4ddf8cc29da1b2ec92ff9d31cc49d89d4a6
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2021-06-10 13:57:47 +0200

    added option in check_config to allow for new arguments in the input config which are missing in the reference config

--- R/check_config.R
+++ R/check_config.R
@@ -1,13 +1,13 @@
 #' Check config
-#' 
+#'
 #' Checks a model configuration file for consistency by comparing it to a
 #' reference config file and the given module structure of the model. The
 #' function will throw out an error if settings are missing in the config which
 #' exist in the reference config, of if settings are set in the config which do
 #' not exist in the reference config file or if a realization is chosen for a
 #' module which does not exist, not allowed setting combinations.
-#' 
-#' 
+#'
+#'
 #' @param icfg Input config which should be checked for consistency (either as
 #' the config itself or as a file path linking to the config)
 #' @param reference_file Reference config which is having the right format
@@ -16,75 +16,92 @@
 #' corresponding module check is deactivated.
 #' @param settings_config path where the table of possible setting combinations
 #' is stored, if NULL it is ignored
+#' @param extras vector of setting names that are allowed to appear in the input config
+#' even if they are missing in the reference config. That can be useful to allow for
+#' additional settings/information which does not necessarily have to exist
 #' @return The checked config as a config list ready for further usage.
 #' @author Jan Philipp Dietrich, Lavinia Baumstark
 #' @seealso \code{\link{getModules}}
 #' @export
 #' @importFrom utils read.csv2
-check_config <- function(icfg,reference_file="config/default.cfg",modulepath="modules/",settings_config=NULL) {
-  
-.source_config <- function(icfg) {
-  cfg <- NULL
-  # set the cfg object
-  if(!is.list(icfg)) {
-    if(is.character(icfg)) {
-      if(file.exists(path("config",icfg))) icfg <- path("config",icfg) 
-      source(icfg,local=TRUE)
-      if(!is.list(cfg)) stop("Wrong input file format: config file does not contain a cfg list!")
-      icfg <- cfg
-      rm(cfg)
-    } else {
-      stop("Wrong input format: cfg is neither a list nor a character!")
+check_config <- function(icfg, reference_file = "config/default.cfg", modulepath = "modules/", #nolint
+                         settings_config = NULL, extras = NULL) {                              #nolint
+
+  .sourceConfig <- function(icfg) {
+    cfg <- NULL
+    # set the cfg object
+    if (!is.list(icfg)) {
+      if (is.character(icfg)) {
+        if (file.exists(path("config", icfg))) icfg <- path("config", icfg)
+        source(icfg, local = TRUE)
+        if (!is.list(cfg)) stop("Wrong input file format: config file does not contain a cfg list!")
+        icfg <- cfg
+        rm(cfg)
+      } else {
+        stop("Wrong input format: cfg is neither a list nor a character!")
+      }
     }
-  }  
-  return(icfg)
-}
+    return(icfg)
+  }
 
 
-  icfg <- .source_config(icfg)
-  cfg  <- .source_config(reference_file) 
-  
-  missing_settings <- names(cfg)[!(names(cfg) %in% names(icfg))]
-  extra_settings   <- names(icfg)[!(names(icfg) %in% names(cfg))]
-  
-  list_elems <- names(cfg)[unlist(lapply(cfg,is.list))]
-  for(l in list_elems) {
+  icfg <- .sourceConfig(icfg)
+  cfg  <- .sourceConfig(reference_file)
+
+  missingSettings <- names(cfg)[!(names(cfg) %in% names(icfg))]
+  extraSettings   <- names(icfg)[!(names(icfg) %in% names(cfg))]
+
+  listElems <- names(cfg)[unlist(lapply(cfg, is.list))]
+  for (l in listElems) {
     tmp <- names(cfg[[l]])[!(names(cfg[[l]]) %in% names(icfg[[l]]))]
-    if(length(tmp)>0) missing_settings <- c(missing_settings,paste(l,tmp,sep="$"))
-    if(is.list(icfg[[l]])) {
-      tmp <- names(icfg[[l]])[!(names(icfg[[l]]) %in% names(cfg[[l]]))]   
-      if(length(tmp)>0) extra_settings <- c(extra_settings,paste(l,tmp,sep="$"))
+    if (length(tmp) > 0) missingSettings <- c(missingSettings, paste(l, tmp, sep = "$"))
+    if (is.list(icfg[[l]])) {
+      tmp <- names(icfg[[l]])[!(names(icfg[[l]]) %in% names(cfg[[l]]))]
+      if (length(tmp) > 0) extraSettings <- c(extraSettings, paste(l, tmp, sep = "$"))
     } else {
-      warning("Setting ",l," is - contrary to the reference config file - not a list!",call.=FALSE)
+      warning("Setting ", l, " is - contrary to the reference config file - not a list!", call. = FALSE)
     }
   }
-  if(length(extra_settings)>0)   warning("Settings are unknown in provided cfg (",paste("cfg",extra_settings,sep="$",collapse=", "),")!",call.=FALSE)
-  if(length(missing_settings)>0) stop("Settings are missing in provided cfg (",paste("cfg",missing_settings,sep="$",collapse=", "),")!",call.=FALSE)
-  
-  #check whether all modules are correctly adressed
-  if(!is.null(modulepath)) {
-    m <- getModules(modulepath=modulepath)
-    for(i in 1:dim(m)[1]) {
-      r <- icfg$gms[m[i,"name"]]
-      if(is.null(r)) stop("No setting found for module \"",m[i,"name"],"\"")
-      if(!(r %in% strsplit(m[i,"realizations"],",")[[1]])) stop("Chosen realization \"",r,"\" does not exist for module \"",m[i,"name"],"\"",call.=FALSE)    
+  if (!is.null(extras)) {
+    extraSettings <- extraSettings[!(sub("\\$.*$", "", extraSettings) %in% extras)]
+  }
+  if (length(extraSettings) > 0) {
+    warning("Settings are unknown in provided cfg (", paste("cfg", extraSettings, sep = "$", collapse = ", "),
+            ")!", call. = FALSE)
+  }
+  if (length(missingSettings) > 0) {
+    stop("Settings are missing in provided cfg (", paste("cfg", missingSettings, sep = "$", collapse = ", "),
+         ")!", call. = FALSE)
+  }
+
+  # check whether all modules are correctly adressed
+  if (!is.null(modulepath)) {
+    m <- getModules(modulepath = modulepath)
+    for (i in 1:dim(m)[1]) {
+      r <- icfg$gms[m[i, "name"]]
+      if (is.null(r)) stop("No setting found for module \"", m[i, "name"], "\"")
+      if (!(r %in% strsplit(m[i, "realizations"], ",")[[1]])) {
+        stop("Chosen realization \"", r, "\" does not exist for module \"", m[i, "name"], "\"", call. = FALSE)
+      }
     }
   }
-    
+
   # check for inconsistent setting combinations
-  if(!is.null(settings_config)){
+  if (!is.null(settings_config)) {
     # Read-in table of possible settings
-    possible_settings <- read.csv2(settings_config,row.names=1,check.names=FALSE)
-    
+    possibleSettings <- read.csv2(settings_config, row.names = 1, check.names = FALSE)
+
     # make list of settings of icfg
-    cfg_gms_list <- paste(names(icfg$gms),"|", icfg$gms,sep="")
-    
-    for (j in intersect(rownames(possible_settings),cfg_gms_list)){
-      notallowed_settings <- colnames(possible_settings)[possible_settings[j,]==0]
-      inconsistent_settings <- intersect(notallowed_settings,cfg_gms_list)
-      if(length(inconsistent_settings) > 0) stop(j, " is not consistent with ",paste(inconsistent_settings,collapse=", "),call.=FALSE)
+    cfgGMSList <- paste(names(icfg$gms), "|", icfg$gms, sep = "")
+
+    for (j in intersect(rownames(possibleSettings), cfgGMSList)) {
+      notallowedSettings <- colnames(possibleSettings)[possibleSettings[j, ] == 0]
+      inconsistentSettings <- intersect(notallowedSettings, cfgGMSList)
+      if (length(inconsistentSettings) > 0) {
+        stop(j, " is not consistent with ", paste(inconsistentSettings, collapse = ", "), call. = FALSE)
+      }
     }
-    
+
   }
   return(icfg)
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 'GAMS' Modularization Support Package
 
-R package **gms**, version **0.9.1**
+R package **gms**, version **0.10.0**
 
 [![CRAN status](https://www.r-pkg.org/badges/version/gms)](https://cran.r-project.org/package=gms) [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.4390032.svg)](https://doi.org/10.5281/zenodo.4390032)  [![R build status](https://github.com/pik-piam/gms/workflows/check/badge.svg)](https://github.com/pik-piam/gms/actions) [![codecov](https://codecov.io/gh/pik-piam/gms/branch/master/graph/badge.svg)](https://codecov.io/gh/pik-piam/gms)
 
@@ -43,9 +43,10 @@ In case of questions / problems please contact Jan Philipp Dietrich <dietrich@pi
 
 To cite package **gms** in publications use:
 
-Dietrich J, Klein D, Giannousakis A, Beier F, Koch J, Baumstark L (2021). _gms: 'GAMS' Modularization
-Support Package_. doi: 10.5281/zenodo.4390032 (URL: https://doi.org/10.5281/zenodo.4390032), R package
-version 0.9.1, <URL: https://github.com/pik-piam/gms>.
+Dietrich J, Klein D, Giannousakis A, Beier F, Koch J, Baumstark L (2021). _gms: 'GAMS'
+Modularization Support Package_. doi: 10.5281/zenodo.4390032 (URL:
+https://doi.org/10.5281/zenodo.4390032), R package version 0.10.0, <URL:
+https://github.com/pik-piam/gms>.
 
 A BibTeX entry for LaTeX users is
 
@@ -54,7 +55,7 @@ A BibTeX entry for LaTeX users is
   title = {gms: 'GAMS' Modularization Support Package},
   author = {Jan Philipp Dietrich and David Klein and Anastasis Giannousakis and Felicitas Beier and Johannes Koch and Lavinia Baumstark},
   year = {2021},
-  note = {R package version 0.9.1},
+  note = {R package version 0.10.0},
   doi = {10.5281/zenodo.4390032},
   url = {https://github.com/pik-piam/gms},
 }
