59a6f58967b9b9649cdb63f4c11903af9cc0bae8
Pascal Führlich <pascal.fuehrlich@pik-potsdam.de>
2021-09-21 16:24:08 +0200

    pass ... to interfaceplot, DoNotPlot in test

--- tests/testthat/test-codeCheck.R
+++ tests/testthat/test-codeCheck.R
@@ -1,17 +1,17 @@
-context("codeCheck test")
-
 test_that("interfaces properly detected and plotted", {
-  expected_result <- list(core = c(out = "pm_global"), 
-                           fancymodule = c(`in` = "pm_global", 
-                                           out = "vm_exchange"), 
-                           crazymodule = c(`in` = "vm_exchange"))
-  expect_message(cc <- codeCheck(system.file("dummymodel",package="gms")),"All codeCheck tests passed!")
-  expect_identical(cc,expected_result)
+  expectedResult <- list(core = c(out = "pm_global"),
+                         fancymodule = c(`in` = "pm_global",
+                                         out = "vm_exchange"),
+                         crazymodule = c(`in` = "vm_exchange"))
+  expect_message({
+    cc <- codeCheck(system.file("dummymodel", package = "gms"))
+  }, "All codeCheck tests passed!")
+  expect_identical(cc, expectedResult)
 })
 
 test_that("return value of codeCheck in debug mode is correct", {
-  expected_result <- c("interfaceInfo","ap","gams","gams_backup","sap","esap","modulesInfo")
+  expectedResult <- c("interfaceInfo", "ap", "gams", "gams_backup", "sap", "esap", "modulesInfo")
 
-  cc <- codeCheck(system.file("dummymodel",package="gms"), debug = TRUE)
-  expect_identical(names(cc),expected_result)
+  cc <- codeCheck(system.file("dummymodel", package = "gms"), debug = TRUE)
+  expect_identical(names(cc), expectedResult)
 })
--- tests/testthat/test-interfaceplot.R
+++ tests/testthat/test-interfaceplot.R
@@ -1,17 +1,15 @@
 context("interface plot test")
 
 test_that("interfaceplot creation works", {
-  cwd <- getwd()
-  on.exit(setwd(cwd))
-  setwd(tempdir())
-  expected_result <- structure(list(from = c("core", "fancymodule"), 
-                                    to = c("fancymodule", "crazymodule"), 
-                                    num_items = c(1L, 1L), 
-                                    items = c("pm_global","vm_exchange")), 
-                               row.names = c(NA, -2L), 
-                               class = c("tbl_df","tbl", "data.frame"))
-  
-  cc <- codeCheck(system.file("dummymodel",package="gms"))
-  ifp <- interfaceplot(cc)
-  expect_identical(ifp,expected_result)
+  withr::local_dir(withr::local_tempdir())
+  expectedResult <- structure(list(from = c("core", "fancymodule"),
+                                    to = c("fancymodule", "crazymodule"),
+                                    num_items = c(1L, 1L),
+                                    items = c("pm_global", "vm_exchange")),
+                               row.names = c(NA, -2L),
+                               class = c("tbl_df", "tbl", "data.frame"))
+
+  cc <- codeCheck(system.file("dummymodel", package = "gms"))
+  ifp <- interfaceplot(cc, DoNotPlot = TRUE)
+  expect_identical(ifp, expectedResult)
 })
