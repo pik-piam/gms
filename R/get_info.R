6e21bac2735a0e2f83c20a127bb038441eee5a40
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2022-02-16 14:55:59 +0100

    - introduced description wrapping in selectScript - removed export statements for get_info and getfiledestinations

--- R/get_info.R
+++ R/get_info.R
@@ -1,25 +1,24 @@
 #' get_info
-#' 
+#'
 #' Function to extract information from info.txt
-#'  
+#'
 #' @param file path to info.txt (including info.txt)
 #' @param grep_expression String before the information that should be extracted
 #' @param sep Separator between grep_expression and information
 #' @param pattern Optional pattern to be replaced (default pattern = "")
 #' @param replacement Optional replacement (default replacement = "")
-#' @export
 #' @author Jan Philipp Dietrich, David Klein
 
-get_info <- function(file, grep_expression, sep, pattern="", replacement=""){
-  if(!file.exists(file)) return(NULL)
-  file <- readLines(file, warn=FALSE)
-  tmp <- grep(grep_expression, file, value=TRUE) # pick line
-  if(identical(tmp,character(0))) return(NA)
+get_info <- function(file, grep_expression, sep, pattern = "", replacement = "") {
+  if (!file.exists(file)) return(NULL)
+  file <- readLines(file, warn = FALSE)
+  tmp <- grep(grep_expression, file, value = TRUE) # pick line
+  if (identical(tmp, character(0))) return(NA)
   tmp <- strsplit(tmp, sep)
   tmp <- sapply(tmp, "[[", 2)
-  tmp <- gsub(pattern, replacement ,tmp)
-  if(all(!is.na(as.logical(tmp)))) return(as.vector(sapply(tmp, as.logical)))
-  if (all(!(regexpr("[a-zA-Z]",tmp) > 0))){
+  tmp <- gsub(pattern, replacement, tmp)
+  if (all(!is.na(as.logical(tmp)))) return(as.vector(sapply(tmp, as.logical)))
+  if (all(!(regexpr("[a-zA-Z]", tmp) > 0))) {
     tmp <- as.numeric(tmp)
   }
   return(tmp)
--- R/selectScript.R
+++ R/selectScript.R
@@ -1,9 +1,9 @@
 #' selectScript
-#' 
+#'
 #' Functions which allows for interactive selection of scripts/files.
-#' 
-#' 
-#' @param folder Folder in which the files/scripts are located which should 
+#'
+#'
+#' @param folder Folder in which the files/scripts are located which should
 #' be selected from.
 #' @param ending File ending of the files to be selected (without dot)
 #' @return A vector of paths to files selected by the user
@@ -12,102 +12,133 @@
 #' @importFrom utils capture.output
 #' @export
 
-selectScript <- function(folder=".", ending="R") {
-  
-  get_line <- function(){
+selectScript <- function(folder = ".", ending = "R") { # nolint
+
+  width <- 70
+
+  .printInfo <- function(info, startNumber = 1, width = 50, maxchar = 30) {
+    descriptionWidth <- width - maxchar - 7
+    info$position <- NULL
+    if (!is.null(info$folder)) info$script <- info$folder
+    info$number <- seq_len(nrow(info)) +  startNumber - 1
+    info$number <- paste0(format(info$number, width = 2, justify = "right"), ": ")
+    splitDescription <- strwrap(info$description, width = descriptionWidth, simplify = FALSE)
+    out <- NULL
+    for (i in seq_len(nrow(info))) {
+      nExtra <- length(splitDescription[[i]]) - 1
+      out <- rbind(out, data.frame(script = c(info$script[i], rep("", nExtra)),
+                                   description = splitDescription[[i]],
+                                   number = c(info$number[i], rep("    ", nExtra))))
+    }
+    message(paste0(out$number, format(gsub("_", " ", out$script, fixed = TRUE), width = maxchar, justify = "right"),
+                   " | ", out$description, collapse = "\n"))
+    return(out)
+  }
+
+  getLine <- function() {
     # gets characters (line) from the terminal or from a connection
     # and returns it
-    if(interactive()){
+    if (interactive()) {
       s <- readline()
     } else {
       con <- file("stdin")
-      s <- readLines(con, 1, warn=FALSE)
-      on.exit(close(con))
+      s <- readLines(con, 1, warn = FALSE)
+      on.exit(close(con)) # nolint
     }
-    return(s);
-  }
-  
-  max_nchar <- function(x,width,prefix="-> ", suffix=" <-", sep="-") {
-    x <- capture.output(cat(strsplit(x," ")[[1]], fill=width))
-    x <- format(x,width=width , justify = "centre")
-    if(!is.null(sep)) {
-      sep <- paste0(" ",paste(rep(sep,nchar(prefix)+nchar(suffix)+nchar(x)[1]),collapse=""))
-    }
-    return(paste(sep,paste0(" ",prefix,x,suffix, collapse="\n"),sep,sep="\n"))
+    return(s)
   }
 
-  subfolders <- list.dirs(folder,full.names = FALSE)
-  subfolders <- subfolders[subfolders!=""]
-  
-  fp <- paste0("\\.",ending,"$")
-  script <- gsub(fp,"",grep(fp,list.files(folder), value=TRUE))
-  if(length(script)==0 && length(subfolders)==0) {
+  maxNchar <- function(x, width, prefix = "-> ", suffix = " <-", sep = "-") {
+    width <- width - 7
+    x <- capture.output(cat(strsplit(x, " ")[[1]], fill = width))
+    x <- format(x, width = width, justify = "centre")
+    if (!is.null(sep)) {
+      sep <- paste0(" ", paste(rep(sep, nchar(prefix) + nchar(suffix) + nchar(x)[1]), collapse = ""))
+    }
+    return(paste(sep, paste0(" ", prefix, x, suffix, collapse = "\n"), sep, sep = "\n"))
+  }
+
+  subfolders <- list.dirs(folder, full.names = FALSE)
+  subfolders <- subfolders[subfolders != ""]
+
+  fp <- paste0("\\.", ending, "$")
+  script <- gsub(fp, "", grep(fp, list.files(folder), value = TRUE))
+  if (length(script) == 0 && length(subfolders) == 0) {
     message("Empty folder selected. NULL returned.")
     return(NULL)
   }
 
-  subinfo <- data.frame(folder=NULL,description=NULL, position=NULL, stringsAsFactors = FALSE)
-  for(s in subfolders) {
-    if(file.exists(file.path(folder,s,"INFO.yml"))) {
-      tmp <- read_yaml(file.path(folder,s,"INFO.yml"))
+  subinfo <- data.frame(folder = NULL, description = NULL, position = NULL, stringsAsFactors = FALSE)
+  for (s in subfolders) {
+    if (file.exists(file.path(folder, s, "INFO.yml"))) {
+      tmp <- read_yaml(file.path(folder, s, "INFO.yml"))
     } else {
       tmp <- list()
     }
-    subinfo <- rbind(subinfo,data.frame(folder      = s,
-                                        description = ifelse(is.null(tmp$description),"",tmp$description),
-                                        position    = ifelse(is.null(tmp$position),NA,tmp$position), 
+    subinfo <- rbind(subinfo, data.frame(folder      = s,
+                                        description = ifelse(is.null(tmp$description), "", tmp$description),
+                                        position    = ifelse(is.null(tmp$position), NA, tmp$position),
                                         stringsAsFactors = FALSE))
-    
+
   }
-  if(nrow(subinfo)>0) subinfo <- subinfo[order(subinfo$position),]
-  
-  #read descriptions in scripts
-  info <- data.frame(script=NULL,description=NULL, position=NULL, stringsAsFactors = FALSE)
-  for(s in script) {
-    tmp <- read_yaml_header(file.path(folder,paste0(s,".",ending)))
-    info <- rbind(info,data.frame(script      = s,
-                                  description = ifelse(is.null(tmp$description),"",tmp$description),
-                                  position    = ifelse(is.null(tmp$position),NA,tmp$position), 
+  if (nrow(subinfo) > 0) {
+    subinfo <- subinfo[order(subinfo$position), ]
+  }
+
+  # read descriptions in scripts
+  info <- data.frame(script = NULL, description = NULL, position = NULL, stringsAsFactors = FALSE)
+  for (s in script) {
+    tmp <- read_yaml_header(file.path(folder, paste0(s, ".", ending)))
+    info <- rbind(info, data.frame(script      = s,
+                                  description = ifelse(is.null(tmp$description), "", tmp$description),
+                                  position    = ifelse(is.null(tmp$position), NA, tmp$position),
                                   stringsAsFactors = FALSE))
   }
-  if(nrow(info)>0) info <- info[order(info$position),]
-  maxchar <- max(nchar(c(info$script,subinfo$folder)))
-  
-  if(file.exists(file.path(folder,"INFO.yml"))) {
-    yaml <- read_yaml(file.path(folder,"INFO.yml"))
+  if (nrow(info) > 0) {
+    info <- info[order(info$position), ]
+  }
+  maxchar <- max(nchar(c(info$script, subinfo$folder)))
+
+  if (file.exists(file.path(folder, "INFO.yml"))) {
+    yaml <- read_yaml(file.path(folder, "INFO.yml"))
   } else {
     yaml <- list()
   }
 
-  if(is.null(yaml$type)) yaml$type  <- "script"
+  if (is.null(yaml$type)) yaml$type  <- "script"
   message("")
-  if(!is.null(yaml$description)) message(yaml$description)
-  if(!is.null(yaml$note)) message(max_nchar(yaml$note,width=40))
-  message(paste0(format(1:nrow(info),width=2, justify="right"), ": ", format(gsub("_"," ",info$script,fixed=TRUE), width=maxchar, justify="right")," | ", info$description, collapse="\n"))
-  if(nrow(subinfo)>0) {
-    message("\nAlternatively, choose a ",yaml$type," from another selection:")
-    message(paste0(format(nrow(info)+(1:nrow(subinfo)),width=2, justify="right"),": ", format(subinfo$folder, width=maxchar, justify="right"), " | ", subinfo$description, collapse="\n"))
+  if (!is.null(yaml$description)) message(yaml$description)
+  if (!is.null(yaml$note)) message(maxNchar(yaml$note, width = width))
+  .printInfo(info, maxchar = maxchar, width = width)
+  if (nrow(subinfo) > 0) {
+    message("\nAlternatively, choose a ", yaml$type, " from another selection:")
+    .printInfo(subinfo, startNumber = nrow(info) + 1, maxchar = maxchar, width = width)
+    message("Choose a ", yaml$type, " or folder: ", appendLF = FALSE)
+  } else {
+    message("Choose a ", yaml$type, ": ", appendLF = FALSE)
   }
-  message("Choose a ",yaml$type,": ",appendLF = FALSE)
-  identifier <- get_line()
-  identifier <- as.numeric(strsplit(identifier,",")[[1]])
-  if(all(identifier==0)) return(NULL)
-  if (any(!(identifier %in% 1:(nrow(info)+nrow(subinfo))))) stop("This choice (",identifier,") is not possible. Please type in a number between 1 and ",(nrow(info)+nrow(subinfo)))
-  if(any(identifier>nrow(info))) {
-    folder_identifier <- identifier[identifier>nrow(info)]
-    identifier <- identifier[identifier<=nrow(info)]
+  identifier <- getLine()
+  identifier <- as.numeric(strsplit(identifier, ",")[[1]])
+  if (all(identifier == 0)) return(NULL)
+  if (any(!(identifier %in% 1:(nrow(info) + nrow(subinfo))))) {
+    stop("This choice (", identifier, ") is not possible. Please type in a number between 1 and ",
+         (nrow(info) + nrow(subinfo)))
   }
-  if(length(identifier)>0) {
-    out <- paste0(info$script[identifier],".",ending)
+  if (any(identifier > nrow(info))) {
+    folderIdentifier <- identifier[identifier > nrow(info)]
+    identifier <- identifier[identifier <= nrow(info)]
+  }
+  if (length(identifier) > 0) {
+    out <- paste0(info$script[identifier], ".", ending)
   } else {
     out <- NULL
   }
-  if(exists("folder_identifier")) {
-    for(fi in folder_identifier) {
-      subfolder <- subinfo$folder[fi-nrow(info)]
-      out <- c(out, file.path(subfolder,selectScript(file.path(folder,subfolder), ending=ending)))
+  if (exists("folderIdentifier")) {
+    for (fi in folderIdentifier) {
+      subfolder <- subinfo$folder[fi - nrow(info)]
+      out <- c(out, file.path(subfolder, selectScript(file.path(folder, subfolder), ending = ending)))
     }
   }
-  if(length(out)==0) return(NULL)
+  if (length(out) == 0) return(NULL)
   return(out)
 }
