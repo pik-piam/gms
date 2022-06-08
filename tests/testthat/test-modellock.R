12bc2f641287676bc521b90452fda18052911e3a
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2022-06-08 14:26:10 +0200

    buildLibrary()

--- tests/testthat/test-modellock.R
+++ tests/testthat/test-modellock.R
@@ -3,7 +3,7 @@ context("model lock/unlock test")
 test_that("standard lock/unlock works", {
   lfolder <- tempdir()
   .lockInOtherSession <- function(lfolder) {
-    callr::r(function(lfolder) gms::model_lock(folder=lfolder, timeout1 = 1e-6), list("lfolder"=lfolder))
+    callr::r(function(lfolder) gms::model_lock(folder = lfolder, timeout1 = 1e-6), list("lfolder" = lfolder))
   }
   lfile <- paste0(lfolder, "/.lock")
 
