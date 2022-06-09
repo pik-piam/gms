7e312fe401ec120e8601df0664d2143b1a1e3ec2
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2022-06-09 15:20:52 +0200

    Add test for basic lock/unlock which works on CI

--- tests/testthat/test-modellock.R
+++ tests/testthat/test-modellock.R
@@ -1,6 +1,14 @@
 context("model lock/unlock test")
 
 test_that("standard lock/unlock works", {
+  lfolder <- tempdir()
+  lfile <- paste0(lfolder, "/.lock")
+  id <- model_lock(folder = lfolder)
+  expect_true(file.exists(lfile))
+  model_unlock(id = id)
+})
+
+test_that("locking protects access from other sessions", {
   skip_on_ci() # launching new R sessions with callr and using renvs during tests is unstable, so disable on ci
   lfolder <- tempdir()
   .lockInOtherSession <- function(lfolder) {
