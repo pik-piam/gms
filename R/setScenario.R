285db398e7d226c118fd71a45584815f007201a9
David Klein <dklein@pik-potsdam.de>
2024-07-25 12:31:33 +0200

    Add option to pass a scenario config as data frame to setScenario()

--- R/setScenario.R
+++ R/setScenario.R
@@ -1,14 +1,15 @@
 #' setScenario
-#' 
+#'
 #' setScenario is adapting a given config to a predefined scenario, meaning
 #' that all settings which are fixed for the given scenario are written to the
 #' config. Settings not defined by the scenario remain unchanged.
-#' 
-#' 
+#'
+#'
 #' @param cfg Input config which should be adapted to the given scenario
 #' @param scenario name of scenario (e.g. "SSP2"). Can also be a vector of
 #' scenarios. In this case scenario settings are applied in the given order
-#' @param scenario_config The path where the scenario config table is stored.
+#' @param scenario_config A data frame containing the scenario config table
+#' or the path where the scenario config table is stored.
 #' @return The updated config as a config list ready for further usage.
 #' @note The scenario config table is a table which contains as columns the
 #' different scenarios and as rows the different settings. Empty entries for a
@@ -19,46 +20,41 @@
 #' @importFrom utils read.csv
 #' @seealso \code{\link{check_config}},\code{\link{getModules}}
 setScenario <- function(cfg,scenario,scenario_config="config/scenario_config.csv"){
-  
-  # setwd("~/0_SVN/MAgPIE_SSP")
-  # source("config/default_test.cfg")
-  # source("config/default.cfg")
-  # scenario <- "n600"
-  # x <- scenario
-  # scenario_config <-"config/scenario_config_test.csv"
-  # scenario_config <-"config/scenario_config.csv"
-  
-    if (!is.list(cfg)) {
-      if (is.character(cfg)) {
-        if (file.exists(path("config", cfg))) 
-          cfg <- path("config", cfg)
-        source(cfg, local = TRUE)
-        if (!is.list(cfg)) 
-          stop("Wrong input file format: config file does not contain a cfg list!")
-      }
-      else {
-        stop("Wrong input format: cfg is neither a list nor a character!")
-      }
+
+  if (!is.list(cfg)) {
+    if (is.character(cfg)) {
+      if (file.exists(path("config", cfg)))
+        cfg <- path("config", cfg)
+      source(cfg, local = TRUE)
+      if (!is.list(cfg))
+        stop("Wrong input file format: config file does not contain a cfg list!")
     }
+    else {
+      stop("Wrong input format: cfg is neither a list nor a character!")
+    }
+  }
+
+  if (!is.data.frame(scenario_config)) {
     tmp <- try(read.csv(scenario_config, as.is = TRUE, check.names = FALSE, colClasses = "character"), silent = TRUE)
-    #check whether reading the table was succesfull, if not try to read it differently
+    #check whether reading the table was successful, if not try to read it differently
     if(inherits(tmp, "try-error") || all(dimnames(tmp)[[1]]==as.character(1:dim(tmp)[1]))) {
       tmp <- read.csv(scenario_config, as.is=TRUE, sep=";", check.names=FALSE, colClasses = "character")
       dimnames(tmp)[[1]] <- tmp[,1]
       tmp <- tmp[,-1]
     }
     scenario_config <- tmp
-    
-    
+  }
+
+
   for (x in scenario) {
     if(!(x %in% colnames(scenario_config))) stop("No settings for scenario ", x, " found in scenario config!")
     message("\nApply ", x, " settings on config:")
-    
+
     # if there are no switches containing "$" all switches are interpreted as gams switches
     if (!any(grepl("\\$",rownames(scenario_config)))) {
       rownames(scenario_config) <- paste0("gms$",rownames(scenario_config))
     }
-    
+
     # loop over switches to chanhe their values
     for(i in rownames(scenario_config)){
       from <- eval(parse(text=paste0("cfg$",i))) # this way, because i can contain a list. In this case access with [[i]] would not work
@@ -70,10 +66,10 @@ setScenario <- function(cfg,scenario,scenario_config="config/scenario_config.csv
         if(grepl(",",to,fixed=TRUE)) to <- strsplit(to,",")[[1]]
         # finally set the switch
         eval(parse(text=paste0("cfg$",i, "<- to")))
-      }   
+      }
     }
     message("Config successfully updated!\n")
   }
-    
-  return(cfg)  
+
+  return(cfg)
 }
