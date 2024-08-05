e3466c85f77557304738319fedf0f4cc10329772
orichters <oliverr@pik-potsdam.de>
2024-08-05 09:36:21 +0200

    allow to specify main file name in readDefaultConfig

--- R/readDefaultConfig.R
+++ R/readDefaultConfig.R
@@ -1,10 +1,12 @@
 #' readDefaultConfig
 #'
-#' Reads the default configuration of the model. Uses default.cfg or main.cfg as the source as appropriate.
+#' Reads the default configuration of the model. Uses default.cfg and the model's main gms
+#' as the source as appropriate.
 #' To read a configuration from YAML format, use \code{\link{loadConfig}} instead.
 #'
 #'
 #' @param path path of the main folder of the model
+#' @param mainfile filename of main model file, defaults to 'main.gms'
 #' @return A vector of parameter values and their names.
 #' @author Mika Pflüger
 #' @seealso \code{\link{loadConfig}}
@@ -12,14 +14,14 @@
 #' @importFrom utils modifyList
 #' @export
 
-readDefaultConfig <- function(path) {
+readDefaultConfig <- function(path = ".", mainfile = "main.gms") {
   # read in settings from main.gms first
   cfg <- list()
-  fileMain <- file.path(path, "main.gms")
+  fileMain <- file.path(path, mainfile)
   if (file.exists(fileMain)) {
     cfg$gms <- as.list(readSettings(fileMain))
   } else {
-    stop("readDefaultConfig cannot find main.gms in ", normalizePath(path))
+    stop("readDefaultConfig cannot find ", mainfile, " in ", normalizePath(path))
   }
   # overwrite with settings from default.cfg
   env <- new.env()
